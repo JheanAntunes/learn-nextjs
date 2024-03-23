293451bce8874080c5a5f8b92b4d7c5f05412e5d
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-22 22:20:33 -0300

    :sparkles:	feat:  (page) add Component PageProducts

---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,12 @@
+import ProductList from "@/components/produtos/components/product-list";
+import React from "react";
+
+const PageProducts = () => {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+};
+
+export default PageProducts;
